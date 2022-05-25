ff05b984db08bb84b121ad2a500e9ce83918b718
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2022-05-25 13:07:20 -0600

    integrated the definitions onto the page

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, seeResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    seeResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
-    alert(`Defining ${keyword}`);
+
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
@@ -24,6 +27,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" autoFocus={true} onChange={handleKeyword} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
