7348b04576b87e6716bdd4833d936911c972f11a
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2022-05-28 14:49:06 -0600

    Added Results and Phonetics css, took out .filter of results

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,13 +1,13 @@
 import React from "react";
+import "./Phonetics.css";
 
 export default function Phonetics(props) {
   return (
     <div className="Phonetics">
+      <p className="text">{props.phonetic.text}</p>
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
-      <br />
-      <p>{props.phonetic.text}</p>
     </div>
   );
 }
--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Meaning from "./Meaning";
 import Phonetics from "./Phonetics";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
@@ -8,17 +9,13 @@ export default function Results(props) {
       <div className="Results">
         <section>
           <h2>{props.results.word}</h2>
-          {props.results.phonetics
-            .filter(function (phonetic) {
-              return phonetic.audio;
-            })
-            .map(function (phonetic, index) {
-              return (
-                <div key={index}>
-                  <Phonetics phonetic={phonetic} />
-                </div>
-              );
-            })}
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetics phonetic={phonetic} />
+              </div>
+            );
+          })}
         </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
