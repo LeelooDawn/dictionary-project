554df425d13311a1cb5729325118d9d0bd8ee252
LeelooDawn <98427940+LeelooDawn@users.noreply.github.com>
2022-05-28 15:09:19 -0600

    updated Phonetics CSS

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -4,10 +4,10 @@ import "./Phonetics.css";
 export default function Phonetics(props) {
   return (
     <div className="Phonetics">
-      <p className="text">{props.phonetic.text}</p>
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         Listen
       </a>
+      <span className="text">{props.phonetic.text}</span>
     </div>
   );
 }
